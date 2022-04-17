dce8a24da2c105673517a1df84f6756361bca2eb
Jay Zhang <wangyoucao577@gmail.com>
2022-04-17 10:41:40 +0800

    feat: correct len

--- medialib/util/read.go
+++ medialib/util/read.go
@@ -9,11 +9,12 @@ import (
 // ReadOrError read a specifed amount of data, otherwise error.
 func ReadOrError(r io.Reader, data []byte) error {
 
+	l := len(data)
 	n, err := r.Read(data)
 	if err != nil {
 		return err
-	} else if n != len(data) {
-		s := fmt.Sprintf("expect to read %d bytes but got %d bytes", len(data), n)
+	} else if n != l {
+		s := fmt.Sprintf("expect to read %d bytes but got %d bytes", l, n)
 		return errors.New(s)
 	}
 
