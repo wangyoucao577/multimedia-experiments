17d9960eba4ddab52eb8b06d79976cd31fe572b7
Jay Zhang <wangyoucao577@gmail.com>
2022-04-19 09:35:56 +0800

    feat: complete stsc box

--- medialib/mp4/box/stsc/stsc_box.go
+++ medialib/mp4/box/stsc/stsc_box.go
@@ -2,6 +2,7 @@
 package stsc
 
 import (
+	"encoding/binary"
 	"io"
 
 	"github.com/golang/glog"
@@ -9,9 +10,19 @@ import (
 	"github.com/wangyoucao577/multimedia-experiments/medialib/util"
 )
 
+// ChunkEntry reprensents a chunk box entry.
+type ChunkEntry struct {
+	FirstChunk             uint32 `json:"first_chunk"`
+	SamplesPerChunk        uint32 `json:"samples_per_chunk"`
+	SampleDescriptionIndex uint32 `json:"sample_description_index"`
+}
+
 // Box represents a stsc box.
 type Box struct {
 	box.FullHeader `json:"full_header"`
+
+	EntryCount uint32
+	Entries    []ChunkEntry
 }
 
 // New creates a new Box.
@@ -35,10 +46,42 @@ func (b *Box) ParsePayload(r io.Reader) error {
 		return err
 	}
 
-	glog.Warningf("box type %s payload bytes %d parsing TODO", b.Type, b.PayloadSize())
-	//TODO: parse payload
-	if err := util.ReadOrError(r, make([]byte, b.PayloadSize())); err != nil {
+	// start to parse payload
+	var parsedBytes uint64
+
+	data := make([]byte, 4)
+	if err := util.ReadOrError(r, data); err != nil {
 		return err
+	} else {
+		b.EntryCount = binary.BigEndian.Uint32(data)
+		parsedBytes += 4
+	}
+
+	for i := 0; i < int(b.EntryCount); i++ {
+		entry := ChunkEntry{}
+
+		if err := util.ReadOrError(r, data); err != nil {
+			return err
+		} else {
+			entry.FirstChunk = binary.BigEndian.Uint32(data)
+			parsedBytes += 4
+		}
+
+		if err := util.ReadOrError(r, data); err != nil {
+			return err
+		} else {
+			entry.SamplesPerChunk = binary.BigEndian.Uint32(data)
+			parsedBytes += 4
+		}
+
+		if err := util.ReadOrError(r, data); err != nil {
+			return err
+		} else {
+			entry.SampleDescriptionIndex = binary.BigEndian.Uint32(data)
+			parsedBytes += 4
+		}
+
+		b.Entries = append(b.Entries, entry)
 	}
 
 	return nil
