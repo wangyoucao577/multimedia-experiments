c2332611a835c9bd682495e801fe839cf1531423
Jay Zhang <wangyoucao577@gmail.com>
2022-04-23 10:01:52 +0800

    feat: complete trun box

--- medialib/mp4/box/trun/trun_box.go
+++ medialib/mp4/box/trun/trun_box.go
@@ -2,6 +2,8 @@
 package trun
 
 import (
+	"encoding/binary"
+	"fmt"
 	"io"
 
 	"github.com/golang/glog"
@@ -12,6 +14,14 @@ import (
 // Box represents a trun box.
 type Box struct {
 	box.FullHeader `json:"full_header"`
+
+	SampleCount                 uint32   `json:"sample_count"`
+	DataOffset                  int32    `json:"data_offset,omitempty"`
+	FirstSampleFlags            uint32   `json:"first_sample_flags,omitempty"`
+	SampleDuration              []uint32 `json:"sample_duration,omitempty"`
+	SampleSize                  []uint32 `json:"sample_size,omitempty"`
+	SampleFlags                 []uint32 `json:"sample_flags,omitempty"`
+	SampleCompositionTimeOffset []int64  `json:"sample_composition_time_offset,omitempty"` // int32 or uint32 in file
 }
 
 // New creates a new Box.
@@ -35,10 +45,81 @@ func (b *Box) ParsePayload(r io.Reader) error {
 		return err
 	}
 
-	glog.Warningf("box type %s payload bytes %d parsing TODO", b.Type, b.PayloadSize())
-	//TODO: parse payload
-	if err := util.ReadOrError(r, make([]byte, b.PayloadSize())); err != nil {
+	// start to parse payload
+	var parsedBytes uint64
+	payloadSize := b.PayloadSize()
+	data := make([]byte, 4)
+
+	if err := util.ReadOrError(r, data); err != nil {
 		return err
+	} else {
+		b.SampleCount = binary.BigEndian.Uint32(data)
+		parsedBytes += 4
+	}
+
+	if (b.Flags & 0x1) > 0 {
+		if err := util.ReadOrError(r, data); err != nil {
+			return err
+		} else {
+			b.DataOffset = int32(binary.BigEndian.Uint32(data))
+			parsedBytes += 4
+		}
+	}
+
+	if (b.Flags & 0x4) > 0 {
+		if err := util.ReadOrError(r, data); err != nil {
+			return err
+		} else {
+			b.FirstSampleFlags = binary.BigEndian.Uint32(data)
+			parsedBytes += 4
+		}
+	}
+
+	for i := 0; i < int(b.SampleCount); i++ {
+		if (b.Flags & 0x100) > 0 {
+			if err := util.ReadOrError(r, data); err != nil {
+				return err
+			} else {
+				sampleDuration := binary.BigEndian.Uint32(data)
+				b.SampleDuration = append(b.SampleDuration, sampleDuration)
+				parsedBytes += 4
+			}
+		}
+
+		if (b.Flags & 0x200) > 0 {
+			if err := util.ReadOrError(r, data); err != nil {
+				return err
+			} else {
+				sampleSize := binary.BigEndian.Uint32(data)
+				b.SampleSize = append(b.SampleSize, sampleSize)
+				parsedBytes += 4
+			}
+		}
+
+		if (b.Flags & 0x400) > 0 {
+			if err := util.ReadOrError(r, data); err != nil {
+				return err
+			} else {
+				sampleFlags := binary.BigEndian.Uint32(data)
+				b.SampleFlags = append(b.SampleFlags, sampleFlags)
+				parsedBytes += 4
+			}
+		}
+
+		if (b.Flags & 0x800) > 0 {
+			if err := util.ReadOrError(r, data); err != nil {
+				return err
+			} else {
+				// don't need to check b.Version since we can use `int64` to compatible for both
+				sampleCompositionTimeOffset := int64(binary.BigEndian.Uint32(data))
+				b.SampleCompositionTimeOffset = append(b.SampleCompositionTimeOffset, sampleCompositionTimeOffset)
+				parsedBytes += 4
+			}
+		}
+	}
+
+	if parsedBytes != payloadSize {
+		return fmt.Errorf("box %s parsed bytes != payload size: %d != %d", b.Type, parsedBytes, payloadSize)
 	}
 
 	return nil
