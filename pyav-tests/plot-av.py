e505bb922f874dd2cf27d6cc47c13eebef0ba5a8
Jay Zhang <wangyoucao577@gmail.com>
2023-10-05 14:03:28 +0800

    feat: use numpy instead of calculation by hand

--- pyav-tests/plot-av.py
+++ pyav-tests/plot-av.py
@@ -2,43 +2,65 @@ import av
 import argparse
 import matplotlib.pyplot as plt
 import os
-
-
-def timestamp2ms(ts, time_base):
-    return float(ts) * time_base * 1000
+import numpy as np
 
 
 class StreamInfo:
     def __init__(self, stream):
         # self.stream = stream
         self.stream_index = stream.index
-        self.time_base = stream.time_base
         self.stream_type = stream.type
+        self.time_base = stream.time_base
 
+        # raw data from packets
         self.dts_array = []
         self.pts_array = []
         self.duration_array = []
         self.size_array = []
 
-        self.dts_delta_array = []  # dts - prev_dts
-
-    def get_ts_array_in_ms(self, timestamps):
-        return [timestamp2ms(t, self.time_base) for t in timestamps]
+        self.data_array = None
 
     def capture_by_packet(self, packet):
-        prev_dts = None
-        if self.dts_array:
-            prev_dts = self.dts_array[-1]
-
         self.dts_array.append(packet.dts)
         self.pts_array.append(packet.pts)
-        if packet.duration is not None:
-            self.duration_array.append(packet.duration)
+        self.duration_array.append(packet.duration)
         self.size_array.append(packet.size)
 
-        if prev_dts is not None:
-            dts_delta = packet.dts - prev_dts
-            self.dts_delta_array.append(dts_delta)
+    def finalize(self):
+        # construct data array
+        self.data_array = np.array(
+            [
+                self.dts_array,
+                self.pts_array,
+                self.duration_array,
+                self.size_array,
+                self.dts_array,  # for dts_delta calculation
+            ],
+            dtype=np.float64,
+        )
+
+        # calculate dts_delta = dts - prev_dts
+        self.data_array[4] = np.roll(self.data_array[4], -1)
+        self.data_array[4][-1] = np.nan  # ignore last value
+        self.data_array[4] = self.data_array[4] - self.data_array[0]
+
+        # [[dts, pts, duration, size], [dts, pts, duration, size], [...]]
+        # self.data_array.transpose()
+
+    def dts_array_in_seconds(self):
+        return self.data_array[0] * self.time_base
+
+    def pts_array_in_seconds(self):
+        return self.data_array[1] * self.time_base
+
+    def duration_array_in_seconds(self):
+        return self.data_array[2] * self.time_base
+
+    def size_array_in_KB(self):
+        return self.data_array[3] / 1024.0
+
+    def dts_delta_in_seconds(self):
+        return self.data_array[4] * self.time_base
 
 
 def plot_av(window_title, v_stream, a_stream):
@@ -53,16 +75,14 @@ def plot_av(window_title, v_stream, a_stream):
     # dts
     axs[0, 0].set_title(f"dts")
     axs[0, 0].set_xlabel("packet no.", loc="right")
-    axs[0, 0].set_ylabel("dts (ms)")
+    axs[0, 0].set_ylabel("dts (s)")
     axs[0, 0].plot(
-        v_stream.get_ts_array_in_ms(v_stream.dts_array),
-        # v_stream.get_ts_array_in_ms(v_stream.dts_array),
+        v_stream.dts_array_in_seconds(),
         VIDEO_LINE_FMT,
         label="video",
     )
     axs[0, 0].plot(
-        a_stream.get_ts_array_in_ms(a_stream.dts_array),
-        # a_stream.get_ts_array_in_ms(a_stream.dts_array),
+        a_stream.dts_array_in_seconds(),
         AUDIO_LINE_FMT,
         label="audio",
     )
@@ -72,18 +92,16 @@ def plot_av(window_title, v_stream, a_stream):
     # pts
     axs[0, 1].set_title(f"pts")
     axs[0, 1].set_xlabel("packet no.", loc="right")
-    axs[0, 1].set_ylabel("pts (ms)")
+    axs[0, 1].set_ylabel("pts (s)")
     # axs[0, 1].yaxis.tick_right()
     # axs[0, 1].yaxis.set_label_position("right")
     axs[0, 1].plot(
-        v_stream.get_ts_array_in_ms(v_stream.pts_array),
-        # v_stream.get_ts_array_in_ms(v_sorted_pts_array),
+        v_stream.pts_array_in_seconds(),
         VIDEO_LINE_FMT,
         label="video",
     )
     axs[0, 1].plot(
-        a_stream.get_ts_array_in_ms(a_stream.pts_array),
-        # a_stream.get_ts_array_in_ms(a_sorted_pts_array),
+        a_stream.pts_array_in_seconds(),
         AUDIO_LINE_FMT,
         label="audio",
     )
@@ -94,12 +112,12 @@ def plot_av(window_title, v_stream, a_stream):
     axs[0, 2].set_xlabel("packet no.", loc="right")
     axs[0, 2].set_ylabel("size (KB)")
     axs[0, 2].plot(
-        [float(s) / 1024 for s in v_stream.size_array],
+        v_stream.size_array_in_KB(),
         VIDEO_LINE_FMT,
         label="video",
     )
     axs[0, 2].plot(
-        [float(s) / 1024 for s in a_stream.size_array],
+        a_stream.size_array_in_KB(),
         AUDIO_LINE_FMT,
         label="audio",
     )
@@ -107,17 +125,17 @@ def plot_av(window_title, v_stream, a_stream):
 
     # dts delta
     axs[1, 0].set_title(f"dts delta")
-    axs[1, 0].set_xlabel("dts (ms)", loc="right")
-    axs[1, 0].set_ylabel("dts_delta (ms)")
+    axs[1, 0].set_xlabel("dts (s)", loc="right")
+    axs[1, 0].set_ylabel("dts_delta (s)")
     axs[1, 0].plot(
-        v_stream.get_ts_array_in_ms(v_stream.dts_array)[1:],
-        v_stream.get_ts_array_in_ms(v_stream.dts_delta_array),
+        v_stream.dts_array_in_seconds(),
+        v_stream.dts_delta_in_seconds(),
         VIDEO_LINE_FMT,
         label="video",
     )
     axs[1, 0].plot(
-        a_stream.get_ts_array_in_ms(a_stream.dts_array)[1:],
-        a_stream.get_ts_array_in_ms(a_stream.dts_delta_array),
+        a_stream.dts_array_in_seconds(),
+        a_stream.dts_delta_in_seconds(),
         AUDIO_LINE_FMT,
         label="audio",
     )
@@ -125,19 +143,19 @@ def plot_av(window_title, v_stream, a_stream):
 
     # duration
     axs[1, 1].set_title(f"duration")
-    axs[1, 1].set_xlabel("dts (ms)", loc="right")
-    axs[1, 1].set_ylabel("duration (ms)")
+    axs[1, 1].set_xlabel("dts (s)", loc="right")
+    axs[1, 1].set_ylabel("duration (s)")
     # axs[1, 1].yaxis.tick_right()
     # axs[1, 1].yaxis.set_label_position("right")
     axs[1, 1].plot(
-        v_stream.get_ts_array_in_ms(v_stream.dts_array),
-        v_stream.get_ts_array_in_ms(v_stream.duration_array),
+        v_stream.dts_array_in_seconds(),
+        v_stream.duration_array_in_seconds(),
         VIDEO_LINE_FMT,
         label="video",
     )
     axs[1, 1].plot(
-        a_stream.get_ts_array_in_ms(a_stream.dts_array),
-        a_stream.get_ts_array_in_ms(a_stream.duration_array),
+        a_stream.dts_array_in_seconds(),
+        a_stream.duration_array_in_seconds(),
         AUDIO_LINE_FMT,
         label="audio",
     )
@@ -164,6 +182,10 @@ def main():
                 continue  # empty packet for flushing, ignore it
             si = streams_info[packet.stream_index]
             si.capture_by_packet(packet)
+
+    # process data
+    for s in streams_info:
+        s.finalize()
 
     # select v/a stream
     v_stream = None
